cc1ff9b98e056a90ebc4cf24d5b2bc841f80e541
María Frausto <mfrausto.fs@gmail.com>
2025-03-28 13:25:25 -0600

    update: logo y enlaces header

--- includes/header.php
+++ includes/header.php
@@ -1,13 +1,18 @@
 <?php ?>
 <header>
+<div class="logo-container">
+    <img src="/img/logo.png" alt="Logo de English 2learn" class="logo">
     <h1>English 2learn</h1>
+</div>
+<div class="nav-bar">
     <nav>
         <ul>
-            <li><a href="pages/index.php">Inicio</a></li>
-            <li><a href="pages/servicios.php">Servicios</a></li>
-            <li><a href="pages/contacto.php">Contacto</a></li>
-            <li><a href="pages/aboutus.php">Sobre nosotros</a></li>
-            <li><a href="pages/blog.php">Blog</a></li>
+            <li><a href="/index.php">Inicio</a></li>
+            <li><a href="/pages/servicios.php">Servicios</a></li>
+            <li><a href="/pages/contacto.php">Contacto</a></li>
+            <li><a href="/pages/aboutus.php">Sobre nosotros</a></li>
+            <li><a href="/pages/blog.php">Blog</a></li>
         </ul>
     </nav>
+</div>
 </header>
